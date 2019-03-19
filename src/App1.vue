7ccd6c1a728c4cfc8d721d3955313d13e6c34867
Goldbeener <duxin@daojia.com>
2019-03-19 21:23:06 +0800

    feat: init project

---
+++ src/App1.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    {{message}}
+    <ul v-for="(item, index) in todoList" :key='index'>
+      <TodoItem :todo='item'/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import TodoItem from './components/TodoItem.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TodoItem
+  },
+  data() {
+    return {
+      message: 'hello vue',
+      todoList: [
+        {
+          text: '苹果'
+        },
+        {
+          text: '石榴'
+        },
+        {
+          text: '其他水果'
+        }
+      ]
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li class="text">{{todo.text}}</li>
+</template>
+
+<script>
+export default {
+  props: ['todo']
+}
+</script>
+
+<style scoped>
+.text {
+  color: #f60;
+  text-align: left;
+}
+</style>
